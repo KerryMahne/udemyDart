9a54f278bb055bc8444a19639c576c981a548be4
Kerry Mahne <kerry.mahne@tretton37.com>
2019-08-25 10:18:47 +0200

    libs and project structure, end of #14

---
+++ librariesAndStructure/index.dart
@@ -0,0 +1,10 @@
+import 'package:Test/test.dart' as test;
+// you need a pubspec.yaml for this and name your project
+// then run pub install in the root
+import 'dart:math' as math;
+
+void main() {
+  print(test.meaningOfLife());
+
+  print(math.pi);
+}
---
+++ librariesAndStructure/lib/test.dart
@@ -0,0 +1,3 @@
+int meaningOfLife() {
+  return 6 * 7;
+}
